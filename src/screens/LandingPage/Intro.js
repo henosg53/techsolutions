1d5897a01b026624de54967754d60f8a27795ecd
henosg53 <36129440+henosg53@users.noreply.github.com>
2023-01-27 04:36:05 -0800

    added some

--- src/screens/LandingPage/Intro.js
+++ src/screens/LandingPage/Intro.js
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 
 function Intro() {
   return (
-    <div className="flex items-center justify-center w-full bg-[url('./assets/images/pattern-v.png')] sm:bg-none bg-fit text-white sm:text-black">
+    <div className="flex items-center justify-center w-full bg-[url('./assets/images/22.jpg')] sm:bg-none bg-fit text-white sm:text-black">
       <div className="sw flex flex-wrap items-center py-10">
         <div className="flex-1 flex flex-col p-4">
           <p className="text-white sm:text-gray-500">Company</p>
